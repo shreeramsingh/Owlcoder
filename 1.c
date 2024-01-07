ccb3e30b92b75df02f7b1eef26e39e969c31a273
shreeramsingh <94845569+shreeramsingh@users.noreply.github.com>
2024-01-07 15:44:55 +0530

    pp
    c program

---
+++ 1.c
@@ -0,0 +1,13 @@
+// sum of two numbers
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter any two integer numbers\n");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("sum of %d and %d is %d\n",a,b,c);
+	getch();
+	return 0;
+}
